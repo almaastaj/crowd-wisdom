c9ee30d871f3cc978472adcff4137e570bdf8cee
Almaas Taj <almaastaj123@gmail.com>
2024-03-01 20:05:10 +0530

    Changes for Deployment and bug fixes

--- src/supabase/supabase.js
+++ src/supabase/supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-console.log(process.env);
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
